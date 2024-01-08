41f1a14ce5919514087825f3d449d95cf72f5a24
Hernan <hernanbp.hb@gmail.com>
2024-01-07 22:52:41 -0300

    paginado

--- functions/src/app/controllers/property.ts
+++ functions/src/app/controllers/property.ts
@@ -1,9 +1,8 @@
 import busboy from "busboy";
 import { Request, Response } from "express";
-import { InitStorage } from "../config/db/init-firebase";
-// import { giveCurrentDateTime } from "../helpers";
+import { InitFirebase, InitStorage } from "../config/db/init-firebase";
+import { giveCurrentDateTime } from "../helpers";
 // import { v4 as uuidv4 } from "uuid";
-// import { InitStorage } from "../config/db/init-firebase";
 
 //@ts-ignore
 const upload = async (req: Request, res: Response) => {
@@ -13,7 +12,9 @@ const upload = async (req: Request, res: Response) => {
     bb.on("file", (name, file, info) => {
       const { filename, encoding, mimeType } = info;
       const bucket = InitStorage();
-      const storagePath = `test/${filename}`;
+      const storagePath = `files/images/${
+        filename + "    " + giveCurrentDateTime()
+      }`;
       const fileUpload = bucket.file(storagePath);
 
       file.pipe(fileUpload.createWriteStream({ contentType: mimeType }));
@@ -47,4 +48,34 @@ const upload = async (req: Request, res: Response) => {
   }
 };
 
-export { upload };
+const getProperties = async (req: Request, res: Response) => {
+  try {
+    const db = InitFirebase().firestore();
+    const propertyRef = db.collection("properties");
+
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const pageSize = parseInt(req.query.pageSize as string, 10) || 10;
+
+    const startAfterDoc = req.query.startAfterDoc as string | undefined;
+
+    let query = propertyRef.orderBy("price").limit(pageSize);
+
+    if (startAfterDoc) {
+      const startAfterSnapshot = await propertyRef.doc(startAfterDoc).get();
+      query = query.startAfter(startAfterSnapshot);
+    } else if (page > 1) {
+      const startAt = (page - 1) * pageSize;
+      query = query.startAt(startAt);
+    }
+
+    const querySnapshot = await query.get();
+    const properties = querySnapshot.docs.map((doc: any) => doc.data());
+
+    res.json({ properties, nextPage: querySnapshot.docs.length === pageSize });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export { upload, getProperties };
--- functions/src/app/routes/property.ts
+++ functions/src/app/routes/property.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { upload } from "../controllers/property";
+import { getProperties, upload } from "../controllers/property";
 
 const router = Router();
 
 router.post("/upload", upload);
 
-router.get("/test", (req, res) => {
-  return res.status(200).json({ message: "todo ok en property" });
-});
+router.get("/", getProperties);
 
 export { router };
