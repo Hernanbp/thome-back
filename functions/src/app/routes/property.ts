29c2b5f3fe97baefe1028a1a1a34a4173b5d162b
Hernan <hernanbp.hb@gmail.com>
2024-01-10 22:08:59 -0300

    cambio nombres endpoints

--- functions/src/app/routes/property.ts
+++ functions/src/app/routes/property.ts
@@ -13,7 +13,7 @@ const router = Router();
 router.post("/create", createProperty);
 router.post("/upload", upload);
 router.get("/getAllProperties", getAllProperties);
-router.get("/:id", getPropertyById);
+router.get("/getPropertyById/:id", getPropertyById);
 router.patch("/update/:id", updateProperty);
 router.delete("/delete/:id", deleteProperty);
 
